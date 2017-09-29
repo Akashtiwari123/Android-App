05c23d8cc2fa204a6dcc33efaad9096ec1b05de3
Akash Tiwari <drakashti@gmail.com>
2017-09-29 13:21:06 +0530

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.users.q;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = ( Button) findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"hello",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
