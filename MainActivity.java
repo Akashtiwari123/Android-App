d419a75efff1050e177cfaef46f6159029fc20b1
Akash Tiwari <drakashti@gmail.com>
2017-10-12 20:52:14 +0530

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,23 +1,48 @@
-package com.example.users.q;
+package com.example.vikas.q;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import static android.R.attr.id;
+import static android.R.attr.x;
+import static com.example.vikas.q.R.id.btnC;
+import static com.example.vikas.q.R.id.btnD;
+import static com.example.vikas.q.R.id.btnOne;
+import static com.example.vikas.q.R.id.button1;
 
+public class MainActivity extends AppCompatActivity {
+TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = ( Button) findViewById(R.id.button1);
-        button.setOnClickListener(new View.OnClickListener() {
+        textView = (TextView)findViewById(R.id.txtCalc);
+        Button btnc = ( Button) findViewById(btnC);
+        btnc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this,"hello",Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        Button btnd = ( Button) findViewById(btnD);
+        btnd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        Button btnone = ( Button) findViewById(btnOne);
+        btnone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("1");
             }
         });
     }
